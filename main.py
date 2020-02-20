bc2c24b8a46a18c2d5fd41ed84a0b0f33943dde0
rankern <victorm.lafuente@gmail.com>
2020-02-20 20:56:28 +0100

    a

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
 import read_file
 
 days = 10
-register = true
+resultLibrerias = []
 
-# Por cada dia
-for day in days:
+numDays, librerias, score = read_file.read_file('a_example.txt')
 
-    # Hay alguien registrandose?
-    if not register:
+print(librerias)
+while numDays > 0:
+    print(numDays)
+    #select MAx beneficio
+    if len(librerias) == 0:
+        break
+    maxS = librerias[0].calc_score(score)
+    index = 0
+    i = 0
+    for L in librerias:
+        if L.calc_score(score) > maxS:
+            index = i
+            maxS = L.calc_score(score)
+        i+= 1
+    libSect = librerias[index]
+    del librerias[index]
+    #todo Eliminar mejores que de tiempo
+    [l.del_book_set(libSect.book_set) for l in librerias ]
+    resultLibrerias += [libSect]
 
-        # Meter a una libreria a registrar
-        # Coger libreria mas beneficiosa
-        # Poner a registrar
-        register = true
+    numDays -= libSect.register_time
+
+
+F = open("result.txt", "w")
+F.write(str(len(resultLibrerias)) + "\n")
+
+for l in resultLibrerias:
+    F.write( str(l.library_index) + " " + str((len(l.book_set))) + "\n")   
+    books = ""
+    for b in l.book_set:
+        books = books + str(b) + " "
     
-    library_register.register_time--
-
-
-
-    # Mandar libros de todas las librerias
-    for library in library_register
-        
-        # Enviar n libros de libreria mas valiosos
-        # Quitar esos libros de la libreria
-        # Quitar esos libros de todas las librerias
-
-    if libraryRegister.register_days == 0:
-        libraries_scanning.append(library_register)
-        register = false
+    F.write(books + "\n")
---
+++ main2.py
@@ -0,0 +1,33 @@
+import read_file
+
+days = 10
+register = true
+
+numDays, librerias, score = read_file.read_file('')
+
+# Por cada dia
+for day in days:
+
+    # Hay alguien registrandose?
+    if not register:
+
+        # Meter a una libreria a registrar
+        # Coger libreria mas beneficiosa
+        # Poner a registrar
+        register = true
+    
+    library_register.register_time--
+
+
+
+    # Mandar libros de todas las librerias
+    for library in library_register
+        
+        # Enviar n libros de libreria mas valiosos
+        # Quitar esos libros de la libreria
+        # Quitar esos libros de todas las librerias
+
+    if libraryRegister.register_days == 0:
+        libraries_scanning.append(library_register)
+        register = false
+
